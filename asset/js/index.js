e878a134a6c90456c8d7f4cd662436e812f465cc
Yasmin Sa <yasmin.rebhi.sakhnini@gmail.com>
2019-12-11 19:46:44 +0100

    Almost finished Contact class
    left to do history model render [method, and listeners]

--- asset/js/index.js
+++ asset/js/index.js
@@ -24,8 +24,8 @@ class BaseHTML {
     logo.innerHTML = 'Contact Book'
 
     //add new contact form
-    let NewContactForm = create('section')
-    NewContactForm.setAttribute('class', 'flex-parent NewContactForm')
+    let newContactForm = create('section')
+    newContactForm.setAttribute('class', 'flex-parent newContactForm')
 
 
     //contact list section
@@ -34,7 +34,8 @@ class BaseHTML {
 
     //append stuff
     mainWrapper.append(logo)
-    mainWrapper.append(NewContactForm)
+    mainWrapper.append(newContactForm)
+    mainWrapper.append(contacts)
     root.append(mainWrapper)
   }
 }
@@ -43,7 +44,7 @@ new BaseHTML()
 
 //Add new contact form 
 if (NEWCONTACT) {
-  class NewContactForm {
+  class newContactForm {
     constructor(htmlSelector) {
       this.rootElement = htmlSelector
       this.state = {
@@ -57,21 +58,23 @@ if (NEWCONTACT) {
       return this.state
     }
 
-    update(newObject) {
-
-      console.warn("I will update my instance state with whatever data you sent in")
-
-
+    update(newObject, action = '') {
+      // console.warn("I will update my instance state with whatever data you sent in")
+      Object.assign(this.state, newObject)
+      if (!action) {
+        this.render()
+      }
+      return;
     }
 
-    delete() {
-
+    clear() {
+      this.state = {
+        name: '',
+        phone: [],
+        email: []
+      }
+      this.render()
     }
-
-    save() {
-
-    }
-
     // let flexChildrenWeapper = create('div')
     render() {
       let createListItems = (type) => {
@@ -92,7 +95,7 @@ if (NEWCONTACT) {
       let root = $(this.rootElement);
       let newContactTemplate =
         `
-          <div class="flex-children NewContactForm">
+          <div class="flex-children newContactForm">
             <div class="inputs">
               <div>
               Name
@@ -111,8 +114,8 @@ if (NEWCONTACT) {
           </div>
           <div class="buttons">
         <button class="newContactCancel">Cancel</button>
-        <button type="button" class="newContactAddPhone">+ phone</button>
-        <button class="newContactAddEmail">+ email</button>
+        <button type="numbeewer" class="newContactAddPhone">+ phone</button>
+        <button type="email" class="newContactAddEmail">+ email</button>
         <button class="newContactSave">Save</button>
       </div>
     </div>
@@ -120,6 +123,204 @@ if (NEWCONTACT) {
       root.innerHTML = newContactTemplate
     }
   }
+  //''''''''''''''''''Y we sent the class .newContactForm here!
+  let newForm = new newContactForm('.newContactForm');
+  // Even Listener for Add new contact form
+  listen('click', '.newContactCancel', e => {
+    newForm.clear()
+  });
 
-  let newForm = new NewContactForm('.NewContactForm');
+  listen('click', '.newContactAddPhone', e => {
+    let number = prompt('please insert your number!')
+    let currentState = newForm.read()
+    if (number) {
+      currentState.phone.push(number)
+      newForm.update(currentState)
+    }
+  })
+
+  listen('click', '.newContactAddEmail', e => {
+    let email = prompt('Bliz insert your email!')
+    let currentState = newForm.read()
+    if (email) {
+      currentState.email.push(email)
+      newForm.update(currentState)
+    }
+  })
+
+  listen('click', '.deleteListItem', e => {
+    let type = e.target.getAttribute('data-type')
+    let key = e.target.getAttribute('data-key')
+    let currenState = newForm.read()
+    currenState[type].splice(key, 1)
+    newForm.update(currenState)
+  })
+
+  listen('click', '.newContactSave', e => {
+    let newContactNameInput = $('.newContactName')
+    let name = newContactNameInput.value
+    if (name) {
+      let currentState = newForm.read()
+      currentState.name = name
+      newForm.update(currentState)
+      let New = new Contact(currentState)
+      store.contacts.push(New.contactData)
+      store.save()
+      newForm.clear()
+    }
+  })
+  listen('input', '.newContactName', e => {
+    let currentState = newForm.read()
+    currentState.name = e.target.value;
+    // We STOP THIS update from re-rendering to NOT lose focus of the input
+    newForm.update(currentState, 'send-in-string-to-NOT-render')
+  })
 }
+
+// End of the even Listener for Add new contact form
+
+// END OF NEW CONTACT FORM CLASS AREA
+
+// NEW CONTACT CLASS AREA
+class Contact {
+  constructor(contactData = {}) {
+    // we need to check if it is a new contact or old one
+    if (contactData.id) {
+      this.contactData = contactData
+      this.render()
+      return
+    } else {
+      // This is a new contact so do this
+      // Assign minimal schema to input data (so maps don't break)
+      let contactSchema = {
+        phone: [],
+        email: []
+      }
+      contactData = Object.assign(contactSchema, contactData)
+      //''''''''''''''''''''
+      //this.contactData = newContact
+      //so contactData is an empty object from the beginig, we object assigned
+      // it to contactSchema object with validation: 
+      //added *id, *history which is the same object contactData and *position 
+      // and when we said ----- this.contactData = newContact
+      //we saved it with this to be able to used in others scoop in the class
+      // also the newContact is a schema object || essential data we declarate
+      // it to avoid breaking map() 
+
+      this.contactData = contactData
+      this.saveNewContact()
+    }
+  }
+  saveNewContact() {
+    if (!this.contactData.name) {
+      return;
+    }
+    //A little validation
+    let newContact = {
+      id: Date.now(),
+      history: [this.contactData],
+      position: 0
+    }
+    //This contact will be saved to DB in the eventhanler that called this
+    //''''''''''''''
+    this.contactData = newContact
+    this.render()
+  }
+  render() {
+    let thisPosition = this.contactData.history[this.contactData.position]
+    let createListItems = (type) => {
+      let html = ''
+      thisPosition[type].map((item, i) => {
+        html += `
+        <li>
+          <div class="flex-row">
+            ${item}  
+          </div>
+        </li>      
+        `
+        //we must return smothing
+      })
+      return html
+    }
+    let html =
+      `
+          <div class="flex-children contacts">
+          <div class="inputs">
+          <div>
+            Name
+            <input type="text" class="ContactName" disabled value="${thisPosition.name}">
+          </div>
+          <div class="newContactPhoneList w-100">
+            <ul>
+              ${createListItems('phone')}
+            </ul>
+          </div>
+          <div class="newContactEmailList w-100">
+            <ul>
+              ${createListItems('email')}
+            </ul>
+          </div>
+        </div>
+        <div class="buttons">
+          <button class="contactDelete" data-key="${this.contactData.id}">Delete</button>
+          <button class="contactShowHistory" data-key="${this.contactData.id}">History</button>
+        </div>
+      </div>
+    `
+    let root = $('.contacts')
+    let div = create('div')
+    div.setAttribute('id', this.contactData.id)
+    div.innerHTML = html
+    root.appendChild(div)
+  }
+}
+
+// LISTENERS FOR Contact CLASS
+function getDataAndKeyFormDatabase(id) {
+  let indexKeyInDatabase
+  let thisDataRecord = store.contacts.filter((dbRecord, index) => {
+    if (dbRecord.id == id) {
+      indexKeyInDatabase = index
+      return true
+    }
+  })
+  return [indexKeyInDatabase, thisDataRecord[0]]
+}
+listen('click', '.contactDelete', e => {
+  let thisId = e.target.getAttribute('data-key')
+  let [key, data] = getDataAndKeyFormDatabase(thisId)
+  store.contacts.splice(key, 1)
+  store.save()
+
+  let deleteThisContact = document.getElementById(thisId)
+  deleteThisContact.remove()
+})
+listen('click', '.contactShowHistory', e => {
+  let thisId = e.target.getAttribute('data-key')
+  console.log(thisId);
+})
+//End of Contact class
+
+//At page load: map contact(if there is any) and send them to Contact class
+store.contacts.map(contact => {
+  new Contact(contact)
+})
+
+//The structure of data in database with it comes to:
+
+// let contactData = {
+//   name: '',
+//   phone: [],
+//   email: []
+// }
+
+//This should be the contact data structure (for versions)
+
+// let Contact = {
+//   id: '828587569',
+//   history: [contactData, contactData, contactData],
+//   position: 2
+// }
+
+// DATABASE: [Contact, Contact, Contact, Contact]
+
